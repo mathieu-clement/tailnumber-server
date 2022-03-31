ec0652b2e6b3fffb776e25062b0ca5e048ca38c2
Mathieu Clement <matthew@edelweiss-software.com>
2022-03-31 21:05:05 +0200

    log throwable with error

--- importer/src/main/kotlin/com/edelweiss/software/tailnumber/server/importer/elastic/ElasticExporter.kt
+++ importer/src/main/kotlin/com/edelweiss/software/tailnumber/server/importer/elastic/ElasticExporter.kt
@@ -66,7 +66,7 @@ class ElasticExporter : KoinComponent {
                                 logger.error("10 errors in a row. Stopping.")
                                 throw t
                             } else {
-                                logger.error("Error inserting record ${registration.registrationId.id}", t)
+                                logger.error("Error inserting record ${registration.registrationId.id}: ${t.message}")
                                 logger.info(registration.toString())
                             }
                         }
