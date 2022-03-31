6b592c21e8eda9eeecdff92adecf24e70647ed46
Mathieu Clement <tiktaktok@users.noreply.github.com>
2022-03-31 10:03:25 +0000

    Fix test-import.sh for FAA

--- scripts/faa/test-import.sh
+++ scripts/faa/test-import.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"/../.. || exit
-./gradlew :importer:importFaa --args="/home/tailnumber/tailnumber-data"
+./gradlew :importer:importFaa --args="/home/tailnumber/tailnumber-data/faa/ReleasableAircraft"
